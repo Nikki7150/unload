0bf83ee92abd9034f88e7a39c7256b76a87b7301
Nikki7150 <kanchan.nishka@gmail.com>
2026-07-26 00:01:35 -0700

    more fixing ui

--- frontend/src/pages/JournalViewer.jsx
+++ frontend/src/pages/JournalViewer.jsx
@@ -1,12 +1,19 @@
+import "./../styles/Dashboard.css";
+import { FaTimes } from "react-icons/fa";
+
 export default function JournalViewer({ note, onBack }) {
     if(!note) return null;
     return (
         <div className="journal-viewer">
-            <button className="back-button" onClick={onBack}>Back to Journals</button>
-            <h1>{note.title}</h1>
-            <p>{note.journal}</p>
-            <p><em>{note.topic}</em></p>
-            <p>{new Date(note.created_at).toLocaleDateString()}</p>
+            <div className="journal-viewer-header">
+                <button className="back-button" onClick={onBack}>
+                    <FaTimes />
+                </button>
+                <p><em>{note.topic}</em></p>
+                <p>{new Date(note.created_at).toLocaleDateString()}</p>
+            </div>
+            <h1 className="journal-viewer-title">{note.title}</h1>
+            <p className="journal-viewer-content">{note.journal}</p>
         </div>
     );
 }
