77f9e9b5bcf4b5d509875e02d8beb5231d5fa4b6
Nikki7150 <kanchan.nishka@gmail.com>
2026-07-31 20:35:48 -0700

    final deployment code

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://unload-kappa.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
